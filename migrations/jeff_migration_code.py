c0706004d46afe309dc0142201ac40f88f056ed3
William Marks Weiler <williamweilercash@gmail.com>
2023-03-22 19:35:53 -0500

    refactored migration script for my database

--- migrations/jeff_migration_code.py
+++ migrations/jeff_migration_code.py
@@ -1,11 +1,12 @@
 import warnings
+from settings import db_user, db_host, db_name
 warnings.simplefilter(action='ignore')
 import pandas as pd
 url = "https://raw.githubusercontent.com/tech-interviews-jigsaw/data-analysis-takehomes/main/1-approaches-for-problems/ecommerce-dataset.csv"
 df = pd.read_csv(url)
-
+breakpoint()
 from sqlalchemy import create_engine
-conn_string = 'postgresql://jeffreykatz@localhost/sample_ecommerce'
+conn_string = f'postgresql://{db_user}@{db_host}/{db_name}'
 
 conn = create_engine(conn_string)
 df.to_sql('raw_transactions', conn, if_exists='replace')
