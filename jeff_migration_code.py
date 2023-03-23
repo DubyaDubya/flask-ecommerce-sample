56ca85a1df99469460815240f4e70be3f443db61
William Marks Weiler <williamweilercash@gmail.com>
2023-03-22 20:15:26 -0500

    made migration script work, db now occupado

--- jeff_migration_code.py
+++ jeff_migration_code.py
@@ -1,11 +1,12 @@
 import warnings
 from settings import db_user, db_host, db_name
-warnings.simplefilter(action='ignore')
+from sqlalchemy import create_engine
 import pandas as pd
+warnings.simplefilter(action='ignore')
+
 url = "https://raw.githubusercontent.com/tech-interviews-jigsaw/data-analysis-takehomes/main/1-approaches-for-problems/ecommerce-dataset.csv"
 df = pd.read_csv(url)
-breakpoint()
-from sqlalchemy import create_engine
+
 conn_string = f'postgresql://{db_user}@{db_host}/{db_name}'
 
 conn = create_engine(conn_string)
